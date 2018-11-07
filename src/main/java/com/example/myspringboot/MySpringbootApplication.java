0722ddf0f1fef4e66cfbf4cfa27014153a4055cc
jiaweigang <jwg15210496128@163.com>
2018-11-07 17:20:45 +0800

    添加忽略文件

--- src/main/java/com/example/myspringboot/MySpringbootApplication.java
+++ src/main/java/com/example/myspringboot/MySpringbootApplication.java
@@ -1,8 +1,12 @@
 package com.example.myspringboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
+@EnableAutoConfiguration
 @SpringBootApplication
 public class MySpringbootApplication {
 
--- src/main/java/com/example/myspringboot/test/HelloController.java
+++ src/main/java/com/example/myspringboot/test/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.myspringboot.test;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @author jwg
  * @version 1.0.0
  */
+@Api(value="测试接口",description="写着玩玩")
 @RestController
+@RequestMapping("/demo")
 public class HelloController {
 
+    @ApiOperation(value = "访问并返回一条问候语句", notes = "可好玩了", httpMethod = "POST")
     @RequestMapping("/hello")
     public String hello(){
         return "hello,this is a springboot demo!";
